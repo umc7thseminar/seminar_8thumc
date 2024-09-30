d0b9f7c18711685eed3ed43796e094d0268e1e0d
cgw-notebook <mangang0713@gmail.com>
2024-09-30 19:55:53 +0900

    feature: 헤쌀/이주형 멤버 정보 입력

--- src/contants/member/memberData.ts
+++ src/contants/member/memberData.ts
@@ -1,20 +1,29 @@
 import { Part } from "@/interfaces/member/memberBox";
 
 export const MEMBERS = [
-    {
-        nickname: "닉네임", /// 본인의 닉네임
-        name: "이름", /// 본인의 이름
-        part: Part.운영진, /// 본인의 파트
-        profile_img_url: "/assets/images/members/default_profile_img.png", /// 본인의 프로필 사진, 동일한 경로로 설정해주세요.
-        title: "description", /// 본인의 한 줄 소개
-        redirect_url: "https://github.com", /// 프로필 클릭 시 이동할 경로
-    },
-    {
-        nickname: "은별",
-        name: "정은지",
-        part: Part.운영진,
-        profile_img_url: "/assets/images/members/eunji_profile.png",
-        title: "안녕하세요. umc 부회장 정은지입니다. :)",
-        redirect_url: "https://github.com/bianbbc87",
-    },
-]
+  {
+    nickname: "닉네임", /// 본인의 닉네임
+    name: "이름", /// 본인의 이름
+    part: Part.운영진, /// 본인의 파트
+    profile_img_url: "/assets/images/members/default_profile_img.png", /// 본인의 프로필 사진, 동일한 경로로 설정해주세요.
+    title: "description", /// 본인의 한 줄 소개
+    redirect_url: "https://github.com", /// 프로필 클릭 시 이동할 경로
+  },
+  {
+    nickname: "은별",
+    name: "정은지",
+    part: Part.운영진,
+    profile_img_url: "/assets/images/members/eunji_profile.png",
+    title: "안녕하세요. umc 부회장 정은지입니다. :)",
+    redirect_url: "https://github.com/bianbbc87",
+  },
+
+  {
+    nickname: "헤쌀", /// 본인의 닉네임
+    name: "이주형", /// 본인의 이름
+    part: Part.WEB, /// 본인의 파트
+    profile_img_url: "/assets/images/members/juhyung_profile_img.png", /// 본인의 프로필 사진, 동일한 경로로 설정해주세요.
+    title: "밥은 먹고 다니는 개발자가 되려 합니다", /// 본인의 한 줄 소개
+    redirect_url: "https://github.com/mangang0713", /// 프로필 클릭 시 이동할 경로
+  },
+];
