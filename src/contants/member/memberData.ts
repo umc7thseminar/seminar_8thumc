ac133f4bc11da514002c3e1ae8fc1222e283caa7
Lee Jihee <129031670+munjji@users.noreply.github.com>
2025-03-25 17:59:17 +0900

    refactor: 지지/이지희 profile image 경로 수정

--- src/contants/member/memberData.ts
+++ src/contants/member/memberData.ts
@@ -1,14 +1,14 @@
 import { Part } from "@/interfaces/member/memberBox";
 
 export const MEMBERS = [
-    {
-        nickname: "몽환",
-        name: "김민환",
-        part: Part.SPRINGBOOT,
-        profile_img_url: "/assets/images/members/내동생.jpeg",
-        title: "안녕하세요. umc 김민환입니다. :)",
-        redirect_url: "https://github.com/rlaalsghks8",
-    },
+  {
+    nickname: "몽환",
+    name: "김민환",
+    part: Part.SPRINGBOOT,
+    profile_img_url: "/assets/images/members/내동생.jpeg",
+    title: "안녕하세요. umc 김민환입니다. :)",
+    redirect_url: "https://github.com/rlaalsghks8",
+  },
   {
     nickname: "닉네임", /// 본인의 닉네임
     name: "이름", /// 본인의 이름
@@ -37,8 +37,8 @@ export const MEMBERS = [
     nickname: "지지", /// 본인의 닉네임
     name: "이지희", /// 본인의 이름
     part: Part.운영진, /// 본인의 파트
-    profile_img_url: "/assets/images/members/jiji.png", /// 본인의 프로필 사진, 동일한 경로로 설정해주세요.
-    title: "하이하이요", /// 본인의 한 줄 소개
+    profile_img_url: "/assets/images/members/jiji.jpg", /// 본인의 프로필 사진, 동일한 경로로 설정해주세요.
+    title: "안녕!!!!하세요!!!!", /// 본인의 한 줄 소개
     redirect_url: "https://github.com/munjji", /// 프로필 클릭 시 이동할 경로
   },
 ];
